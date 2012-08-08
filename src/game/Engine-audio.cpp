aaafe53a58351aa8de606a90fae6d49eb7b3eab2
Bruno <bruno.sie.li@gmail.com>
2012-08-08 00:29:21 -0700

    added Mix_OpenAudio

--- src/game/Engine-audio.cpp
+++ src/game/Engine-audio.cpp
@@ -4,6 +4,22 @@
 
 /*
 ================================
+Audio settings.
+
+TODO: Do we need anything more involved?
+Consider mobile devices as well as PC settings.
+================================
+*/
+	// static const int AUDIO_FREQUENCY = 22050;
+	static const int AUDIO_FREQUENCY = 44100;
+static const Uint16 AUDIO_FORMAT = MIX_DEFAULT_FORMAT;
+	static const int AUDIO_MONO = 1;
+	static const int AUDIO_STEREO = 2;
+static const int AUDIO_CHANNELS = AUDIO_STEREO;
+static const int AUDIO_CHUNK_SIZE = 1024;
+
+/*
+================================
 Engine::initAudio
 ================================
 */
@@ -16,7 +32,22 @@ bool Engine::initAudio()
 	}
 
 	if ( Mix_Init( MIX_INIT_OGG ) == -1 ) {
+		std::cerr << "Mix_Init failed: "
+			<< SDL_GetError() << std::endl;
 		return false;
+	}
+
+	if ( Mix_OpenAudio( AUDIO_FREQUENCY, AUDIO_FORMAT, AUDIO_CHANNELS, AUDIO_CHUNK_SIZE ) == -1 ) {
+		std::cerr << "Mix_OpenAudio failed: "
+			<< SDL_GetError() << std::endl;
+		return false;
+	}
+
+	int frequency; Uint16 format; int channels;
+	Mix_QuerySpec( &frequency, &format, &channels );
+	if ( frequency != AUDIO_FREQUENCY || channels != AUDIO_CHANNELS ) {
+		std::cerr << "Engine::initAudio:" << std::endl;
+		std::cerr << "Warning: Actual audio format does not match specified audio format." << std::endl;
 	}
 
 	return true;
@@ -24,5 +55,7 @@ bool Engine::initAudio()
 
 void Engine::cleanupAudio()
 {
+	Mix_CloseAudio();
+
 	Mix_Quit();
 }
